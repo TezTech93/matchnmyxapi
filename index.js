9169f021b20031eefeb03744a98a5b98319bc16c
TezTech93 <129575654+TezTech93@users.noreply.github.com>
2024-01-03 12:30:26 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'users';
+const eventsDb = 'events';
+const groupsDb = 'groups';
+const userSetttingsDb = 'settings';
+const userInboxDb = 'inbox';
+
+// Create Express app
+const app = express();
+app.use(express.json());
+
+//Get Client
+function getClient() {
+    return MongoClient.connect(url);
+}
+
+// Route for getting data from MongoDB
+app.get('/data', async (req, res) => {
+    try {
+        // Connect to MongoDB
+        var client = getClient()
+        console.log('Connected successfully to server');
+        const db = client.db(dbName);
+        const collection = db.collection('myCollection');
+        const documents = await collection.find({}).toArray();
+        console.log('Documents:', documents);
+        client.close();
+        res.json(documents);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+//Route for getting events from MongoDB
+app.get('/events', async (req,res) => {
+    try {
+        var client = getClient();
+        var db = client.db(eventsDb);
+        var collection = db.collection('events');
+        const events = await collection.find({}).toArray();
+        client.close();
+        res.json(events);
+    }catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+//Route for getting groups from MongoDB
+app.get('/groups', async (req,res) => {
+    try {
+        var client = getClient();
+        var db = client.db(groupsDb);
+        var collection = db.collection('groups');
+        const groups = await collection.find({}).toArray();
+        client.close();
+        res.json(events);
+    }catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/users', async (req,res) => {
+    try {
+        var client = getClient();
+        var db = client.db(usersDb);
+        var collection = db.collection('users');
+        const users = await collection.find({}).toArray();
+        client.close();
+        res.json(users);
+    }catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/settings', async (req,res) => {
+    try {
+        var client = getClient();
+        var db = client.db(userSetttingsDb);
+        var collection = db.collection('settings');
+        const settings = await collection.find({}).toArray();
+        client.close();
+        res.json(settings);
+    }catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+
+app.get('/inbox', async (req,res,userId) => {
+    try {
+        var client = getClient();
+        var db = client.db(userInboxDb);
+        var collection = db.collection('inbox');
+        const inbox = await collection.findOne(userId).toArray();
+        client.close();
+        res.json(inbox);
+    }catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
